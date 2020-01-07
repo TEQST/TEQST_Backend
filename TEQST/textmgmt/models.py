8d7773b356533e302b1e777f67d1643c329072ce
YAWNICK <jaweiss1999@googlemail.com>
2020-01-07 23:15:15 +0100

    feat: create Folder class with attributes and __str__ method

--- TEQST/textmgmt/models.py
+++ TEQST/textmgmt/models.py
@@ -2,4 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Folder(models.Model):
+    name = models.CharField(max_length=50)
+    # maybe useful:
+    # limit_choices_to={'groups__name': 'Publisher'}
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folder')  
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='subfolder', blank=True, null=True)
+
+    # this method is useful for the shell, but has no real use in the project
+    def __str__(self):
+        return self.name    
