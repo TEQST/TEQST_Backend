975ad953df013f66a7ab377f92ac8e59dd11883d
YAWNICK <jaweiss1999@googlemail.com>
2020-01-08 14:50:49 +0100

    feat: create app recordingmgmt and register app in settings

--- TEQST/TEQST/settings.py
+++ TEQST/TEQST/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'textmgmt.apps.TextmgmtConfig',
+    'recordingmgmt.apps.RecordingmgmtConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
