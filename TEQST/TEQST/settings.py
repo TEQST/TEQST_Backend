886eb494dc730d2b3c360ef7f090d91384ddb345
Dominik <ueqnn@student.kit.edu>
2020-01-09 15:31:35 +0100

    feat: Add custom User model
    also extends the forms and updates the admin perspective

--- TEQST/TEQST/settings.py
+++ TEQST/TEQST/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = 'usermgmt.CustomUser'
---
+++ TEQST/usermgmt/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = '__all__'
