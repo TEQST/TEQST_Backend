b19dbdbb5dc6df8d6a6c59eddf8da9f7bc4e2bbc
YAWNICK <jaweiss1999@googlemail.com>
2020-01-09 11:39:00 +0100

    feat: create classes sharedfolder and text with their attributes and some methods

--- TEQST/textmgmt/models.py
+++ TEQST/textmgmt/models.py
@@ -6,9 +6,49 @@ class Folder(models.Model):
     name = models.CharField(max_length=50)
     # maybe useful:
     # limit_choices_to={'groups__name': 'Publisher'}
+    # TODO rethink on_delete
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folder')  
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='subfolder', blank=True, null=True)
 
-    # this method is useful for the shell, but has no real use in the project
+    # this method is useful for the shell and for the admin view
     def __str__(self):
-        return self.name    
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        # TODO if not exists yet: create actual Folder
+        super().save(*args, **kwargs)
+    
+    def delete(self, *args, **kwargs):
+        # TODO do not delete actual folder. Except maybe if it is empty.
+        super().delete(*args, **kwargs)
+
+
+class SharedFolder(Folder):
+    speaker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sharedfolder')
+
+    # __str__() is inherited
+
+    def save(self, *args, **kwargs):
+        # actual folder is created in super method
+        # TODO create other necessary stuff like temp folders etc.
+        super().save(*args, **kwargs)
+
+
+def upload_path():
+    # TODO implement
+    pass
+
+
+class Text(models.Model):
+    title = models.CharField(max_length=30)
+    sentences_count = models.IntegerField(null=True)
+    sentences = models.TextField(null=True)
+    shared_folder = models.ForeignKey(SharedFolder, on_delete=models.CASCADE, related_name='text')
+    textfile = models.FileField(upload_to=upload_path)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        # TODO open file, fill fields sentences, sentences_count, close file
+        super().save(*args, **kwargs)
