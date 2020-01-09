fd594372f8b01ca355652297f110d4bb8f6362b7
Dominik <ueqnn@student.kit.edu>
2020-01-09 19:03:54 +0100

    feat: add language and tag models

--- TEQST/usermgmt/admin.py
+++ TEQST/usermgmt/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Language, Tag
 
 class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['username']
     fieldsets = UserAdmin.fieldsets
-    fieldsets[1][1]['fields'] = ('first_name', 'last_name', 'email', 'date_of_birth')
+    fieldsets[1][1]['fields'] = ('first_name', 'last_name', 'email', 'date_of_birth', 'language')
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Language)
+admin.site.register(Tag)
--- TEQST/usermgmt/models.py
+++ TEQST/usermgmt/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True)
+class Language(models.Model):
+    native_name = models.CharField(max_length=50)
+    english_name = models.CharField(max_length=50)
+    short = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.english_name + ' (' + self.native_name + ')'
 
 class Tag(models.Model):
-    pass
+    identifier = models.CharField(max_length=10)
+    default_color = models.CharField(max_length=10)
 
-class Language(models.Model):
-    pass
+    def __str__(self):
+        return self.identifier
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    language = models.ManyToManyField(Language, blank=True)
+
