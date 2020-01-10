9947cb6b3ef3a47fec1a91c76e8bbe07004dc71c
Dominik <ueqnn@student.kit.edu>
2020-01-10 10:49:51 +0100

    feat: add all manytomany relations

--- TEQST/usermgmt/forms.py
+++ TEQST/usermgmt/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta(UserChangeForm.Meta):
--- TEQST/usermgmt/models.py
+++ TEQST/usermgmt/models.py
@@ -18,5 +18,22 @@ class Tag(models.Model):
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
+    education = models.CharField(max_length=50, blank=True, null=True)
     language = models.ManyToManyField(Language, blank=True)
+
+    #Below is not core funcionality
+    tag_usage = models.ManyToManyField(Tag, through='Usage', related_name='Publisher', blank=True)
+    tag_coloring = models.ManyToManyField(Tag, through='Customization', related_name='Einsprecher', blank=True)
+
+
+class Usage(models.Model):
+    #TODO maybe limit_choices_to publisher if it works properly
+    publisher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    meaning = models.CharField(max_length=200)
+
+class Customization(models.Model):
+    einsprecher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    custom_color = models.CharField(max_length=10)
 
