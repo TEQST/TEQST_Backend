de404f4e377ab9bdc6750e3bb88da163de2968d2
YAWNICK <jaweiss1999@googlemail.com>
2020-01-10 22:02:02 +0100

    feat: add textmgmt urls to project urls

--- TEQST/TEQST/urls.py
+++ TEQST/TEQST/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import urls
+
+version = 1
+version_str = 'v' + str(version) + '/'
 
 urlpatterns = [
+    path('api/' + version_str, include('textmgmt.urls')),
     path('admin/', admin.site.urls),
 ]
