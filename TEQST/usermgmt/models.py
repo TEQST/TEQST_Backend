cfff8057bbd834c853cad042729318c3ffa495ff
YAWNICK <jaweiss1999@googlemail.com>
2020-01-14 11:50:29 +0100

    feat: add field gender in CustomUser

--- TEQST/usermgmt/models.py
+++ TEQST/usermgmt/models.py
@@ -17,6 +17,7 @@ class Tag(models.Model):
         return self.identifier
 
 class CustomUser(AbstractUser):
+    gender = models.CharField(max_length=20, null=True, blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     education = models.CharField(max_length=50, blank=True, null=True)
     language = models.ManyToManyField(Language, blank=True)
