5bac0dba58888be9472c5cf831d124783b800fd7
YAWNICK <jaweiss1999@googlemail.com>
2020-01-16 11:22:50 +0100

    feat: merge master into usermgmt

---
+++ TEQST/textmgmt/serializers.py
@@ -0,0 +1,83 @@
+from rest_framework import serializers
+from .models import Folder, SharedFolder, Text
+
+################################
+# important todos:
+# - sharedfolder speaker add/rem
+# - add folder name change functionality
+################################
+
+
+class FolderPKField(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        # TODO sharedfolders should not be allowed to be parent folders for other folders
+        user = self.context['request'].user
+        queryset = Folder.objects.filter(owner=user, sharedfolder=None)
+        return queryset
+
+
+class FolderFullSerializer(serializers.ModelSerializer):
+    """
+    to be used by view: FolderListView
+    for: Folder creation, subfolder list retrieval
+    """
+    parent = FolderPKField()
+    class Meta:
+        model = Folder
+        fields = ['id', 'name', 'owner', 'parent', 'subfolder']
+        read_only_fields = ['owner', 'subfolder']
+
+
+class FolderBasicSerializer(serializers.ModelSerializer):
+    """
+    to be used by view: FolderDetailedView
+    for: Folder update
+    """
+    class Meta:
+        model = Folder
+        fields = ['id']  # add 'name' for folder name change
+
+
+class SharedFolderSerializer(serializers.ModelSerializer):
+    # TODO add read only field of path (callable)
+    """
+    to be used by view: SharedFolderListView, FolderDetailedView
+    for: SharedFolder list retrieval in speaker view, edit (e.g. speaker set) by publisher
+    """
+    class Meta:
+        model = SharedFolder
+        fields = ['id', 'name', 'owner', 'parent', 'speaker']
+        read_only_fields = ['owner', 'parent']
+
+
+class SharedFolderPKField(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        user = self.context['request'].user
+        queryset = Folder.objects.filter(owner=user, subfolder=None)
+        return queryset
+
+
+class TextFullSerializer(serializers.ModelSerializer):
+    """
+    to be used by view: TextDetailedView, TextListView
+    for: opening a text, creation of a text
+    """
+    content = serializers.ListField(source='get_content', child=serializers.DictField(), read_only=True)
+    shared_folder = SharedFolderPKField()
+    class Meta:
+        model = Text
+        # TODO maybe make the textfile write only
+        fields = ['id', 'title', 'shared_folder', 'content', 'textfile']
+        read_only_fields = ['content']
+
+
+class TextBasicSerializer(serializers.ModelSerializer):
+    """
+    to be used by view: TextListView, TextDetailedView
+    for: retrieving a list of texts, updating the title of a text
+    """
+    class Meta:
+        model = Text
+        # TODO adjust fields
+        fields = ['id', 'title']
+        
--- TEQST/textmgmt/views.py
+++ TEQST/textmgmt/views.py
@@ -1,3 +1,93 @@
 from django.shortcuts import render
+from .serializers import FolderFullSerializer, FolderBasicSerializer, SharedFolderSerializer
+from .serializers import TextBasicSerializer, TextFullSerializer
+from .models import Folder, SharedFolder, Text
+from rest_framework import generics, mixins
 
-# Create your views here.
+################################
+# important todos:
+# - the get_queryset method from textlistview
+# - test if sharedfolderbypublisherview works
+################################
+
+
+class FolderListView(generics.ListCreateAPIView):
+    queryset = Folder.objects.all()
+    serializer_class = FolderFullSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if 'parent' in self.request.query_params:
+            #if parent is a sharedfolder: error message
+            return Folder.objects.filter(parent=self.request.query_params['parent'], owner=user.pk)
+        return Folder.objects.filter(parent=None, owner=user.pk)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FolderDetailedView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    """
+    Update Mixin: Folder name change
+    Delete Mixin: Folder deletion
+    """
+    queryset = Folder.objects.all()
+    serializer_class = FolderBasicSerializer
+
+    # not sure if this is really necessary
+    def get_queryset(self):
+        user = self.request.user
+        return Folder.objects.filter(owner=user.pk)
+
+    # the get method and the retreivemodelmixin are only here for debug reasons
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class SharedFolderByPublisherView(generics.ListAPIView):
+    queryset = SharedFolder.objects.all()
+    serializer_class = SharedFolderSerializer
+
+    def get_queryset(self):
+        # TODO test if this works
+        user = self.request.user
+        shares_folders = SharedFolder.objects.filter(speaker=user.pk)
+        return shares_folders.filter(owner=self.kwargs['pub_pk'])
+
+
+class TextListView(generics.ListCreateAPIView):
+    queryset = Text.objects.all()
+    serializer_class = TextBasicSerializer
+
+    def get_queryset(self):
+        # TODO IMPORTANT: Rethink this
+        user = self.request.user
+        if 'sharedfolder' in self.request.query_params:
+            return Text.objects.filter(shared_folder=self.request.query_params['sharedfolder'])
+        # TODO this cant be the alternative. The 'sharedfolder' query param must be required
+        return Text.objects.all()
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return TextFullSerializer
+        return TextBasicSerializer
+
+
+class TextDetailedView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Text.objects.all()
+    serializer_class = TextFullSerializer
+    # TODO make fields sharedfolder, textfile read only; add field content (callable)
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return TextFullSerializer
+        return TextBasicSerializer
