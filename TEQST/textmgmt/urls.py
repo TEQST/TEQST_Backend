b99231806dd41b83302cb742b3c16ea6d4ecf895
YAWNICK <jaweiss1999@googlemail.com>
2020-01-19 18:15:37 +0100

    feat: add view and serializer for sharedfolder speaker change

--- TEQST/textmgmt/urls.py
+++ TEQST/textmgmt/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from .views import FolderListView, FolderDetailedView, SharedFolderByPublisherView, TextListView, TextDetailedView
+from .views import FolderListView, FolderDetailedView, SharedFolderByPublisherView, SharedFolderSpeakerView, TextListView, TextDetailedView
 
 
 urlpatterns = [
     path('folders/', FolderListView.as_view()),
     path('folders/<int:pk>/', FolderDetailedView.as_view()),
+    #path('publishers/', )
     path('publishers/<int:pub_pk>/sharedfolders/', SharedFolderByPublisherView.as_view()),
-    path('texts/', TextListView.as_view()),
+    path('texts/', TextListView.as_view()),  # maybe path('sharedfolders/:id/texts/')
+    path('sharedfolders/<int:pk>/speakers/', SharedFolderSpeakerView.as_view()),
     path('texts/<int:pk>/', TextDetailedView.as_view())
 ]
