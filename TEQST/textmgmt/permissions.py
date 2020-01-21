ed1ebf5c70f9e12b2905b08a1e3b2856335bd84b
YAWNICK <jaweiss1999@googlemail.com>
2020-01-21 11:00:33 +0100

    feat: delete permissions.py in textmgmt because they werent actually necessary to this point

--- TEQST/textmgmt/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-#example. atm not used.
-class IsTextOwnerPermission(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-
-        return obj.shared_folder.owner == request.user
