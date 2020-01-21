5e69f1eb91c9a79c20a2dff8c0529d41e82c7815
YAWNICK <jaweiss1999@googlemail.com>
2020-01-21 09:57:13 +0100

    feat: split api/texts/ into api/pub/texts/ and api/spk/texts/

--- TEQST/textmgmt/urls.py
+++ TEQST/textmgmt/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from .views import FolderListView, FolderDetailedView, SharedFolderByPublisherView, SharedFolderSpeakerView, TextListView, TextDetailedView
+from .views import FolderListView, FolderDetailedView, SharedFolderByPublisherView, SharedFolderSpeakerView, PublisherTextListView, SpeakerTextListView, TextDetailedView
 
 
 urlpatterns = [
     path('folders/', FolderListView.as_view()),
     path('folders/<int:pk>/', FolderDetailedView.as_view()),
-    #path('publishers/', )
-    path('publishers/<int:pub_pk>/sharedfolders/', SharedFolderByPublisherView.as_view()),
-    path('texts/', TextListView.as_view()),  # maybe path('sharedfolders/:id/texts/')
-    path('sharedfolders/<int:pk>/speakers/', SharedFolderSpeakerView.as_view()),
+    path('sharedfolders/', SharedFolderByPublisherView.as_view()),
+    path('pub/texts/', PublisherTextListView.as_view()),
+    path('spk/texts/', SpeakerTextListView.as_view()),
+    path('sharedfolders/<int:pk>/', SharedFolderSpeakerView.as_view()),
     path('texts/<int:pk>/', TextDetailedView.as_view())
 ]
--- TEQST/textmgmt/views.py
+++ TEQST/textmgmt/views.py
@@ -60,9 +60,13 @@ class SharedFolderByPublisherView(generics.ListAPIView):
 
     def get_queryset(self):
         # TODO test if this works
+        # publisher query param should be mandatory
         user = self.request.user
-        shares_folders = SharedFolder.objects.filter(speaker=user.pk)
-        return shares_folders.filter(owner=self.kwargs['pub_pk'])
+        shared_folders = SharedFolder.objects.filter(speaker=user.pk)
+        if 'publisher' in self.request.query_params:
+            return shared_folders.filter(owner=self.request.query_params['publisher'])
+        #this should never be reached
+        return shared_folders
 
 
 class SharedFolderSpeakerView(generics.RetrieveUpdateAPIView):
@@ -72,9 +76,12 @@ class SharedFolderSpeakerView(generics.RetrieveUpdateAPIView):
     queryset = SharedFolder.objects.all()
     serializer_class = SharedFolderSpeakerSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        return SharedFolder.objects.filter(owner=user.pk)
 
 
-class TextListView(generics.ListCreateAPIView):
+class PublisherTextListView(generics.ListCreateAPIView):
     queryset = Text.objects.all()
     serializer_class = TextBasicSerializer
 
@@ -82,14 +89,31 @@ class TextListView(generics.ListCreateAPIView):
         # TODO IMPORTANT: Rethink this
         user = self.request.user
         if 'sharedfolder' in self.request.query_params:
-            return Text.objects.filter(shared_folder=self.request.query_params['sharedfolder'])
-        # TODO this cant be the alternative. The 'sharedfolder' query param must be required
-        return Text.objects.all()
+            if SharedFolder.objects.get(pk=self.request.query_params['sharedfolder']).owner == user:
+                return Text.objects.filter(shared_folder=self.request.query_params['sharedfolder'])
+
+        # TODO The 'sharedfolder' query param must be required.
+        # better solution would maybe be bad response or error
+        return Text.objects.none()
     
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return TextFullSerializer
         return TextBasicSerializer
+
+
+class SpeakerTextListView(generics.ListAPIView):
+    queryset = Text.objects.all()
+    serializer_class = TextBasicSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if 'sharedfolder' in self.request.query_params:
+            if user in SharedFolder.objects.get(pk=self.request.query_params['sharedfolder']).speaker.all():
+                return Text.objects.filter(shared_folder=self.request.query_params['sharedfolder'])
+        
+        # TODO maybe theres a better alternative 
+        return Text.objects.none()
 
 
 class TextDetailedView(generics.RetrieveUpdateDestroyAPIView):
