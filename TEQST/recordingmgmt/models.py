69ec0d075eb6ef0a86d78716558ee9ff27132cba
Dominik <ueqnn@student.kit.edu>
2020-01-22 18:58:36 +0100

    feat: redo upload_to functions

--- TEQST/recordingmgmt/models.py
+++ TEQST/recordingmgmt/models.py
@@ -6,16 +6,19 @@ class TextRecording(models.Model):
     speaker = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     text = models.ForeignKey(Text, on_delete=models.CASCADE)
     # is the audiofile really needed?
-    audiofile = models.FileField(null=True, blank=True)
+    audiofile = models.FileField(upload_to=text_rec_upload_path, null=True, blank=True)
 
     def active_sentence(self):
         return SenctenceRecording.objects.filter(recording=self).count()
 
+#May be needed in a future version
+def text_rec_upload_path(instance, filename):
+    sf_path = instance.recording.text.shared_folder.sharedfolder.get_path()
+    return sf_path + '/AudioData/' + instance.text.title + '_' + instance.speaker.username + '.wav'
 
 def sentence_rec_upload_path(instance, filename):
     sf_path = instance.recording.text.shared_folder.sharedfolder.get_path()
-    # it is probably better to discard the filename and create our own
-    return sf_path + '/TempAudio/' + filename
+    return sf_path + '/TempAudio/' + instance.recording.text.title + '_' + instance.recording.speaker.username + '_' + instance.index + '.wav'
 
 
 class SenctenceRecording(models.Model):
