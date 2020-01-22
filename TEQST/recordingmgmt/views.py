eec70ce465a657fb22c0b8b05a77d41d039ea059
Dominik <ueqnn@student.kit.edu>
2020-01-22 18:46:01 +0100

    feat: add senctence-recording views

--- TEQST/recordingmgmt/views.py
+++ TEQST/recordingmgmt/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, mixins
-from .serializers import TextRecordingSerializer
+from django.db.models import return_None
+from .serializers import TextRecordingSerializer, SenctenceRecordingCreateSerializer, SenctenceRecordingUpdateSerializer
 from .models import TextRecording, SenctenceRecording
 
 
@@ -12,3 +13,17 @@ class TextRecordingView(generics.ListCreateAPIView):
         if 'text' in self.request.query_params:
             return TextRecording.objects.filter(text=self.request.query_params['text'], speaker=user.pk)
         return TextRecording.objects.none()
+
+class SenctenceRecordingCreateView(generics.CreateAPIView):
+    queryset = SenctenceRecording.objects.all()
+    serializer_class = SenctenceRecordingCreateSerializer
+
+class SenctenceRecordingUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = SenctenceRecording.objects.all()
+    serializer_class = SenctenceRecordingUpdateSerializer
+
+    def get_object(self):
+        rec = self.kwargs['rec']
+        if 'index' in self.request.query_params:
+            return SenctenceRecording.objects.get(recording__id=rec, index=self.request.query_params['index'])
+        return models.return_None
