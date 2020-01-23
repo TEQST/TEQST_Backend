4302ffdd614010c298130e2e6d8d8dad80e51fc2
Dominik <ueqnn@student.kit.edu>
2020-01-23 11:41:55 +0100

    feat: language handling also works when registering a new user

--- TEQST/usermgmt/serializers.py
+++ TEQST/usermgmt/serializers.py
@@ -22,17 +22,16 @@ class UserFullSerializer(serializers.ModelSerializer):
 #        except KeyError:
 #            print('No context')
 #        
-    languages = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True)
-    #languages = LanguageSerializer(many = True, read_only = True)
-    #language_ids = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True, source='languages', write_only=True)
+    #languages = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True)
+    languages = LanguageSerializer(many = True, read_only = True)
+    language_ids = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True, source='languages', write_only=True)
     
     is_publisher = serializers.BooleanField(read_only=True)  # source kwarg not needed, because name is same
 
     class Meta():
         model = CustomUser
         fields = ['id', 'username', 'education', 'gender', 'date_of_birth', 'languages', 'language_ids', 'is_publisher']
-        read_only_fields = ['id', 'username', 'is_publisher', 'languages']
-        write_only_fields = ['language_ids']
+        read_only_fields = ['id', 'username', 'is_publisher']
 
     # def update(self, instance, validated_data):
     #     languages_data = validated_data.pop('languages')
@@ -69,13 +68,19 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     Used for user ceration. Maybe this serializer can be integrated in another serializer later on.
     Other fields should probably be added.
     """
+
+    languages = LanguageSerializer(many = True, read_only = True)
+    language_ids = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True, source='languages', write_only=True)
+
     class Meta:
         model = CustomUser
         # languages still need to be added
-        fields = ['username', 'password', 'education', 'gender', 'date_of_birth']
+        fields = ['username', 'password', 'education', 'gender', 'date_of_birth', 'language_ids', 'languages']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
     
     def create(self, validated_data):
+        language_ids = validated_data.pop('languages')
         user = CustomUser.objects.create_user(**validated_data)
+        user.languages.set(language_ids)
         user.save()
         return user
