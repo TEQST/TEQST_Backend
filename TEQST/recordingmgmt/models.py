0a6546aa23afd4914d197a5778dcdb600682ba71
YAWNICK <jaweiss1999@googlemail.com>
2020-01-27 15:14:20 +0100

    feat: add a bit of stm creation logic

--- TEQST/recordingmgmt/models.py
+++ TEQST/recordingmgmt/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from usermgmt.models import CustomUser
 from textmgmt.models import Text
 from .storages import OverwriteStorage
+from .utils import create_textrecording_stm
 
 #May be needed in a future version
 def text_rec_upload_path(instance, filename):
@@ -31,3 +32,14 @@ class SenctenceRecording(models.Model):
     recording = models.ForeignKey(TextRecording, on_delete=models.CASCADE)
     index = models.IntegerField(default=0)
     audiofile = models.FileField(upload_to=sentence_rec_upload_path, storage=OverwriteStorage())
+
+    def save(self, *args, **kwargs):
+        # This should work, but I'm not 100% sure. 
+        # Alternative: self.recording.active_sentence() == self.recording.text.sentence_count()
+        is_update = self.pk
+        super().save(*args, **kwargs)
+        # check is this is the last sentence recording in a text or if this sentence recording is being updated
+        if self.index == self.recording.text.sentence_count() or is_update:
+            # trigger stm creation
+            create_textrecording_stm(self.recording.pk)
+
