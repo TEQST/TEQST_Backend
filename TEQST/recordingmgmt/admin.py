90f8372df290a798e3b76b1eaa24b751997da42a
YAWNICK <jaweiss1999@googlemail.com>
2020-01-28 14:25:07 +0100

    feat: register Textrecording and Sentencerecording to the admin site. Should obviously be removed later

--- TEQST/recordingmgmt/admin.py
+++ TEQST/recordingmgmt/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TextRecording, SentenceRecording
 
 # Register your models here.
+
+admin.site.register(TextRecording)
+admin.site.register(SentenceRecording)
