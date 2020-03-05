b37ce3d1906ccb366f7a728e18b09c720be08762
YAWNICK <jaweiss1999@googlemail.com>
2020-03-05 12:49:17 +0100

    fix: add menu language validation method to userregisterserializer

--- TEQST/usermgmt/serializers.py
+++ TEQST/usermgmt/serializers.py
@@ -92,3 +92,9 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         if value < 1900 or value > date.today().year:
             raise serializers.ValidationError("Invalid birth_year.")
         return value
+    
+    #checks if the given menu_language_id belongs to a menu language
+    def validate_menu_language_id(self, value):
+        if not value.is_menu_language():
+            raise serializers.ValidationError("Invalid menu language.")
+        return value
