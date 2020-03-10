3402e726e7da77202aba023b183f74b1405b3819
YAWNICK <jaweiss1999@googlemail.com>
2020-03-10 13:12:02 +0100

    feat: add authentication and authorization tests to api/folders/ and api/folders/:id/

--- TEQST/textmgmt/tests/test_views.py
+++ TEQST/textmgmt/tests/test_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from usermgmt.tests.utils import *
 from django.contrib.auth.models import Group
 from textmgmt.models import Folder, SharedFolder, Text
+from usermgmt.models import CustomUser
 
 
 class TestFolderStructure(TestCase):
@@ -17,8 +18,7 @@ class TestFolderStructure(TestCase):
         super().setUpClass()
         setup_languages()
         Group.objects.create(name='Publisher')
-        setup_user(USER_DATA_CORRECT_1, make_publisher=True)
-        setup_user(USER_DATA_CORRECT_2)
+        setup_users()  # 1 and 3 are publishers, 2 and 4 are not
 
     def setUp(self):
         self.client = Client()
@@ -34,6 +34,40 @@ class TestFolderStructure(TestCase):
     def test_folders_no_auth(self):
         response = self.client.get(reverse("folders"))
         self.assertEqual(response.status_code, 401)
+        owner = CustomUser.objects.get(username=USER_DATA_CORRECT_1['username']).pk
+        response = self.client.post(reverse("folders"), data={'name': 'f1', 'owner': owner})
+        self.assertEqual(response.status_code, 401)
     
     def test_folder_detail_no_auth(self):
-        pass
+        # setup
+        Folder.objects.create(name='f1', owner=CustomUser.objects.get(username=USER_DATA_CORRECT_1['username']))
+        # test
+        response = self.client.get(reverse("folder-detail", args=[1]))
+        self.assertEqual(response.status_code, 401)
+        response = self.client.put(reverse("folder-detail", args=[1]))
+        self.assertEqual(response.status_code, 401)
+        response = self.client.delete(reverse("folder-detail", args=[1]))
+        self.assertEqual(response.status_code, 401)
+    
+    def test_folders_user_is_not_a_publisher(self):
+        response = self.client.get(reverse("folders"), HTTP_AUTHORIZATION=self.token_2)
+        self.assertEqual(response.status_code, 403)
+        owner = CustomUser.objects.get(username=USER_DATA_CORRECT_2['username']).pk
+        response = self.client.post(reverse("folders"), data={'name': 'f1', 'owner': owner}, HTTP_AUTHORIZATION=self.token_2)
+        self.assertEqual(response.status_code, 403)
+    
+    def test_folder_detail_user_is_not_a_publisher(self):
+        # setup
+        Folder.objects.create(name='f1', owner=CustomUser.objects.get(username=USER_DATA_CORRECT_1['username']))
+        # test
+        response = self.client.get(reverse("folder-detail", args=[1]), HTTP_AUTHORIZATION=self.token_2)
+        self.assertEqual(response.status_code, 403)
+        response = self.client.put(reverse("folder-detail", args=[1]), HTTP_AUTHORIZATION=self.token_2)
+        self.assertEqual(response.status_code, 403)
+        response = self.client.delete(reverse("folder-detail", args=[1]), HTTP_AUTHORIZATION=self.token_2)
+        self.assertEqual(response.status_code, 403)
+    
+    def test_folders_empty(self):
+        response = self.client.get(reverse("folders"), HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 0)
