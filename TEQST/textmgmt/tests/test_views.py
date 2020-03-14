c7d621865819c2e7c6a3a3944df13c18dc367cde
YAWNICK <jaweiss1999@googlemail.com>
2020-03-14 16:49:25 +0100

    feat: add tests for api/pub/texts/?sharedfolder=123 GET Method

--- TEQST/textmgmt/tests/test_views.py
+++ TEQST/textmgmt/tests/test_views.py
@@ -255,7 +255,7 @@ class TestFolderDetailedView(TestCase):
 class TestTextUpload(TestCase):
     """
     urls tested:
-    /api/pub/texts/
+    /api/pub/texts/ POST
     """
 
     @classmethod
@@ -276,16 +276,16 @@ class TestTextUpload(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        path = settings.MEDIA_ROOT + '/harry/'
-        if (os.path.exists(path)):
-            shutil.rmtree(path)
         Folder.objects.filter(name="sharedfolder").delete()
         super().tearDownClass()
 
     def tearDown(self):
+        for user in [USER_DATA_CORRECT_1, USER_DATA_CORRECT_3]:
+            path = settings.MEDIA_ROOT + '/' + user['username'] + '/'
+            if (os.path.exists(path)):
+                shutil.rmtree(path)
         sf = Folder.objects.get(name="Sharedfolder")
         Text.objects.filter(shared_folder=sf).delete()
-        super().tearDown()
     
     def test_correct_upload(self):
         sf = Folder.objects.get(name="Sharedfolder")
@@ -326,6 +326,92 @@ class TestTextUpload(TestCase):
         with open('testtext.txt') as fp:
             response = self.client.post(reverse("pub-texts"), data={'title': "testtext", 'shared_folder': sf.pk, 'textfile': fp})
         self.assertEqual(response.status_code, 401)
+
+
+class TestPublisherTextList(TestCase):
+    """
+    urls tested:
+    /api/pub/texts/?sharedfolder=123 GET
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        setup_languages()
+        Group.objects.create(name='Publisher')
+        setup_users()  # 1 and 3 are publishers, 2 and 4 are not
+    
+    def setUp(self):
+        self.client = Client()
+        login_data_1 = {"username": USER_DATA_CORRECT_1['username'],
+                        "password": USER_DATA_CORRECT_1['password']}
+        login_response_1 = self.client.post(reverse("login"), data=login_data_1)
+        self.token_1 = 'Token ' + login_response_1.json()['token']
+        login_data_2 = {"username": USER_DATA_CORRECT_2['username'],
+                        "password": USER_DATA_CORRECT_2['password']}
+        login_response_2 = self.client.post(reverse("login"), data=login_data_2)
+        self.token_2 = 'Token ' + login_response_2.json()['token']
+    
+    def tearDown(self):
+        for user in [USER_DATA_CORRECT_1, USER_DATA_CORRECT_3]:
+            path = settings.MEDIA_ROOT + '/' + user['username'] + '/'
+            if (os.path.exists(path)):
+                shutil.rmtree(path)
+    
+    def test_pub_text_list_no_auth(self):
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': 1})
+        self.assertEqual(response.status_code, 401)
+
+    def test_pub_text_list_user_is_not_a_publisher(self):
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': 1}, HTTP_AUTHORIZATION=self.token_2)
+        self.assertEqual(response.status_code, 403)
+
+    def test_pub_text_list_correct_empty(self):
+        # setup
+        user1 = CustomUser.objects.get(username=USER_DATA_CORRECT_1['username'])
+        f1 = Folder.objects.create(name='f1', owner=user1)
+        t1 = Text.objects.create(title='test', shared_folder=f1, textfile='test_resources/testtext.txt')
+        t1.delete()
+        self.assertEqual(len(Text.objects.all()), 0)
+        # test
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': f1.pk}, HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 0)
+
+    def test_pub_text_list_correct_with_content(self):
+        # setup
+        user1 = CustomUser.objects.get(username=USER_DATA_CORRECT_1['username'])
+        f1 = Folder.objects.create(name='f1', owner=user1)
+        Text.objects.create(title='test', shared_folder=f1, textfile='test_resources/testtext.txt')
+        Text.objects.create(title='test2', shared_folder=f1, textfile='test_resources/testtext2.txt')
+        # test
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': f1.pk}, HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+
+    def test_pub_text_list_without_sharedfolder(self):
+        response = self.client.get(reverse("pub-texts"), HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 404)
+
+    def test_pub_text_list_folder_does_not_exist(self):
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': 99}, HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 404)
+
+    def test_pub_text_list_invalid_folder(self):
+        # setup
+        user3 = CustomUser.objects.get(username=USER_DATA_CORRECT_3['username'])
+        f1 = Folder.objects.create(name='f1', owner=user3)
+        Text.objects.create(title='test', shared_folder=f1, textfile='test_resources/testtext.txt')
+        # test
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': f1.pk}, HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 404)
+
+    def test_pub_text_list_folder_is_not_shared(self):
+        # setup
+        user1 = CustomUser.objects.get(username=USER_DATA_CORRECT_1['username'])
+        f1 = Folder.objects.create(name='f1', owner=user1)
+        # test
+        response = self.client.get(reverse("pub-texts"), data={'sharedfolder': f1.pk}, HTTP_AUTHORIZATION=self.token_1)
+        self.assertEqual(response.status_code, 404)
 
 
 class TestPublisherListView(TestCase):
