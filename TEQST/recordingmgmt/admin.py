debf9aeb930cd6924959ba333ea2506fd5e1a7f8
YAWNICK <jaweiss1999@googlemail.com>
2020-03-17 16:41:31 +0100

    chore: remove  most models from the admin page

--- TEQST/recordingmgmt/admin.py
+++ TEQST/recordingmgmt/admin.py
@@ -2,6 +2,3 @@ from django.contrib import admin
 from .models import TextRecording, SentenceRecording
 
 # Register your models here.
-
-admin.site.register(TextRecording)
-admin.site.register(SentenceRecording)
